aa8ccf4715d55a506b36ef3d31bf9023e3017c68
Sebastian Llamuca <llamucasebas@gmail.com>
2024-04-27 12:50:51 -0500

    refactor(product-modal-detail): :art: improve the design

--- src/pages/product/modals/ProductModal.tsx
+++ src/pages/product/modals/ProductModal.tsx
@@ -29,14 +29,14 @@ const ProductModal = ({ idProduct, isOpen, closeModal }: ProductModalProps) => {
                 className={`absolute size-4 animate-ping rounded-full ${data?.status ? 'bg-green-500' : 'bg-red-500'}`}
               />
               <div
-                className={`absolute size-4 rounded-full ${data?.status ? 'bg-green-500' : 'bg-red-500'}`}
+                className={`size-4 rounded-full ${data?.status ? 'bg-green-500' : 'bg-red-500'}`}
               />
             </div>
             <AlertDialogTitle className='text-center text-xl'>
               {data.name}
             </AlertDialogTitle>
             <div className='grid grid-cols-2 gap-6'>
-              <div className='space-y-2'>
+              <div className='space-y-4'>
                 <ul className='flex gap-2'>
                   {data.categoryList.map(category => (
                     <li key={category}>
@@ -45,20 +45,22 @@ const ProductModal = ({ idProduct, isOpen, closeModal }: ProductModalProps) => {
                   ))}
                 </ul>
                 <p>{data.description}</p>
-                <p className='font-semibold'>
-                  Precio: <span className='font-normal'>S/.{data.price}</span>
-                </p>
-                <div className='flex gap-2'>
-                  <p className='font-semibold'>Valoración:</p>
-                  <p className='flex items-center gap-1'>
-                    {data.valoration} <Star className='text-yellow-400' />
+                <div className='flex space-x-6'>  
+                  <p className='font-semibold'>
+                    Precio: <span className='font-normal'>S/.{data.price}</span>
                   </p>
+                  <div className='flex gap-2'>
+                    <p className='font-semibold'>Valoración:</p>
+                    <p className='flex items-center gap-1'>
+                      {data.valoration} <Star className='text-yellow-400' />
+                    </p>
+                  </div>
                 </div>
                 {/* Images */}
                 <div className='w-full space-y-2'>
                   <p className='font-semibold'>Imagenes:</p>
                   <Carousel className='w-full'>
-                    <CarouselContent>
+                    <CarouselContent className='items-center'>
                       {data.cardImage && (
                         <CarouselItem className='flex h-full w-full basis-1/4 items-center justify-center'>
                           <img
@@ -95,7 +97,7 @@ const ProductModal = ({ idProduct, isOpen, closeModal }: ProductModalProps) => {
                 </div>
               </div>
               <div className='w-full rounded-md bg-slate-200 p-2'>
-                <ul className='grid grid-cols-2 gap-x-1'>
+                <ul className={`grid grid-cols-2 gap-x-1 ${!(data.stock.length > 0) ? 'h-full' : '' }`}>
                   {data.stock.length > 0 ? (
                     data.stock.map(({ quantity, store }, index) => (
                       <li
@@ -110,7 +112,7 @@ const ProductModal = ({ idProduct, isOpen, closeModal }: ProductModalProps) => {
                       </li>
                     ))
                   ) : (
-                    <div className='relative col-span-2 flex flex-col items-center justify-center py-2 text-slate-400'>
+                    <div className='relative col-span-2 flex flex-col items-center justify-center py-2 text-slate-400 '>
                       <div>
                         <Building className='mx-auto' />
                         <p className='select-none text-center'>No hay stock</p>
