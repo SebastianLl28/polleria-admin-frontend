47baae86898b235033f7b2987782404d760f80c4
Llamuca Dueñas Sebastián <llamucasebas@gmail.com>
2024-06-23 12:17:25 -0500

    style(badge): :art: remove hover

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -8,15 +8,11 @@ const badgeVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
-        secondary:
-          'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        destructive:
-          'border-transparent bg-destructive text-white hover:bg-destructive/80',
+        default: 'border-transparent bg-primary text-primary-foreground',
+        secondary: 'border-transparent bg-secondary text-secondary-foreground',
+        destructive: 'border-transparent bg-destructive text-white',
         outline: 'text-foreground',
-        success:
-          'border-transparent bg-success text-success-foreground hover:bg-success/80'
+        success: 'border-transparent bg-success text-success-foreground'
       },
       size: {
         default: 'text-xs px-2.5 py-0.5',
